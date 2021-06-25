ed69e0891ef1782f4925b3ec9722fdb7c6715f6f
Michael Wender <michael@michaelwender.com>
2021-06-25 12:01:57 -0400

    Setting WC breadcrumbs separator

--- functions.php
+++ functions.php
@@ -25,3 +25,13 @@ function hello_elementor_child_setup_theme(){
 	add_theme_support( 'post-thumbnails' );
 }
 add_action( 'after_setup_theme', 'hello_elementor_child_setup_theme', 99 );
+
+/**
+ * Change the WooCommerce breadcrumb separator.
+ */
+function wcc_change_breadcrumb_delimiter( $defaults ) {
+	// Change the breadcrumb delimeter from '/' to '>'
+	$defaults['delimiter'] = ' &raquo; ';
+	return $defaults;
+}
+add_filter( 'woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_delimiter' );
