1398c4ab8716a29c4009e9af35d8b6bf58e82e73
Michael Wender <michael@michaelwender.com>
2022-05-31 10:36:46 -0400

    Saves a users first access of a rental

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -11,5 +11,11 @@ function parent_theme_enqueue_styles() {
     array( 'hello-elementor-style' )
   );
 
+  wp_register_script( 'rentals', OAKSMIN_THEME_URL . 'lib/js/rentals.js', null, filemtime( OAKSMIN_THEME_PATH . 'lib/js/rentals.js'), true );
+  wp_localize_script( 'rentals', 'wpApiSettings', [
+    'ep'      => rest_url( 'oaksmin/v1/rentals' ),
+    'loader'  => OAKSMIN_THEME_URL . 'lib/img/oaksmin-loader.gif',
+    'nonce'   => wp_create_nonce( 'wp_rest' ),
+  ]);
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\parent_theme_enqueue_styles' );
